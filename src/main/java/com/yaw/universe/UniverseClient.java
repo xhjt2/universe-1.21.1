02d5d9561f89e7437e5134933355fc8d04667894
xhjt <xhjt2@outlook.com>
2025-07-01 02:36:41 +0800

    Dev

---
+++ src/main/java/com/yaw/universe/UniverseClient.java
@@ -0,0 +1,10 @@
+package com.yaw.universe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UniverseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/yaw/universe/UniverseDataGenerator.java
@@ -0,0 +1,11 @@
+package com.yaw.universe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UniverseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
